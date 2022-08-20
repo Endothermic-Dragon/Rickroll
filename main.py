e64da5d5c5c31f9b43fa0f30ca68d158cfadb2e3
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-08-19 23:14:21 -0400

    Working on volume adjustment (or rather, not)

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import os
 import subprocess
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.call("pyw threaded_run.pyw")
+
+subprocess.call("pyw music.pyw")
+subprocess.call("pyw volume.pyw")
